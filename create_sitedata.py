52dadce34ad5bdf81df133d77ab675e2dc9de683
fechan <edu.fechan@gmail.com>
2020-10-07 18:11:16 -0700

    add more fields and sort by time

--- create_sitedata.py
+++ create_sitedata.py
@@ -7,15 +7,36 @@ filename_file = open("filelist.txt", "r")
 filenames = [filename.replace("\n", "") for filename in filename_file.readlines()]
 filename_file.close()
 
-site_data = []
+utterances = []
 for filename in filenames:
-    site_data.append(
+    tokens = filename.split("_")
+    utterances.append(
         {
+            "season":       int(filename[1]),
+            "episode":      int(filename[3]),
+            "discourse":    int(tokens[1].replace("discourse", "")),
+            "time":         float(tokens[2]),
             "audio":        filename + ".mp3",
             "spectrogram":  filename + ".png",
             "meaning":      ""
         }
     )
 
+site_data = {}
+for utterance in utterances:
+    episode_name = f"PIC {utterance['season']}x{str(utterance['episode']).zfill(2)}"
+    if episode_name in site_data:
+        site_data[episode_name]["utterances"].append(utterance)
+    else:
+        site_data[episode_name] = {
+            "episode_name": episode_name,
+            "utterances": []
+        }
+
+sorted_site_data = {}
+for episodeName, episodeData in site_data.items():
+    episodeData["utterances"] = sorted(episodeData["utterances"], key=lambda utterance: utterance["time"])
+    sorted_site_data[episodeName] = episodeData
+
 with open("site_data.json", "w") as f:
-    json.dump(site_data, f, indent=2)
+    json.dump(list(sorted_site_data.values()), f, indent=2)
